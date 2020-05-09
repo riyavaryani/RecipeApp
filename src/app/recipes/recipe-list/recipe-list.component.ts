a32f2c9fa9a70aca0da4ba278d71f56e4b9d0364
= <riyavaryani01@gmail.com>
2020-05-09 19:39:51 +0530

    error in recipe-item html file

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe} from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A test', 'it is only a test recipe', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg'),
   new Recipe('A test', 'it is only a test recipe', 'https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg')
@@ -14,6 +15,10 @@ recipes: Recipe[] = [
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
